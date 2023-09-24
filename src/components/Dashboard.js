5e9ac48d55da4f5ca1e66518da6212719d55afd0
Rauber Simoes do Nascimento <raubersn@gmail.com>
2023-09-24 11:13:26 -0700

    Add state to handle two different views of the dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -29,20 +29,29 @@ const data = [
 
 class Dashboard extends Component {
   state = {
-    loading: false
+    loading: false,
+    focused: null
   };
-
+  
+  selectPanel(id) {
+    this.setState(previousState => ({
+     focused: (previousState.focused ? null : id)
+    }));
+  }
+  
   render() {
-    const dashboardClasses = classnames("dashboard");
+    const dashboardClasses = classnames("dashboard", {
+      "dashboard--focused": this.state.focused
+     });
 
     return (this.state.loading ?
       <Loading /> :
       <main className={dashboardClasses} >
-        {data.map(x => <Panel
-          key={x.id}
-          id={x.id}
-          label={x.label}
-          value={x.value}
+        {(this.state.focused ? data.filter(panel => this.state.focused === panel.id) : data).map(panel => <Panel
+          key={panel.id}
+          label={panel.label}
+          value={panel.value}
+          onSelect={() => this.selectPanel(panel.id)}
         />)}
       </main>);
   }
